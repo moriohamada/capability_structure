99b38408646c2e433ed50869b105f88be209d60a
morio <moriohamada@gmail.com>
2026-02-16 14:50:00 +0000

    added notebook and config comments

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ PLOTS_PATH = "./plots/"
 ANALYSIS_OPS = {
     "minModelsPerBench": 30,
     "minBenchmarksPerModel": 8,
-    "nNullIter": 50,
-    "kNN_k": 5,
+    "nNullIter": 500,
+    "kNN_k": 5,                     # this doesnt seem to change too much in 3-8 range. using 5 like epoch.
     "sigThresh": 0.01,
-    "nTopCreators": 3,
+    "nTopCreators": 3,              # for creating dummies. just only 3 creators had 5+ models
 }
